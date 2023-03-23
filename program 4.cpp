0c01488b0a4aad500e94ba474ceb443dce827117
ashritha6325 <123553039+ashritha6325@users.noreply.github.com>
2023-03-23 21:24:37 +0530

    Create program 4.cpp

---
+++ program 4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x=10;
+    int y=70;
+  
+    x = x + Y;
+    y = x - y;
+    x = x - y;
+  
+    cout <<"x : "<< x <<"\n";
+    cout <<"y : "<< y <<"\n";
+  
+    return 0;
+}
