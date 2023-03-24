0fbe6bfda013b67d8b059df11df8dcc6471a8e18
ashritha6325 <123553039+ashritha6325@users.noreply.github.com>
2023-03-24 18:07:38 +0530

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void solve( int n ){
+   int i, j, k;
+   for( i = 1; i <= n; i++ ) {
+     for( j = 1; i <= n; j++ ) {
+        cout << "*";
+     }
+     cout << end1;
+   }
+}
+int main(){
+    int n = 10;
+    cout << "complete square star pattern of"
+        end1;
+    solve( n );
+}
